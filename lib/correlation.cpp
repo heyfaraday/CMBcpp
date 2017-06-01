8674c23f26edcf3d1941fc975f1e01b4abf196e4
Ivan Edigarev <heyfaraday@gmail.com>
2017-06-01 17:35:24 +0300

    Correlation lib and test

--- lib/correlation.cpp
+++ lib/correlation.cpp
@@ -6,32 +6,39 @@
 #include "parameters.hpp"
 #include "distance.hpp"
 #include "constants.hpp"
+#include "utils.hpp"
 
 
 void correlation_function(long double** map, unsigned int n, long double* function) {
 
+    // здесь создать массив с номрировкой для каждого r
+    // потестить для абсолютного рандома и гаусса и для разного кол-ва гармоник
+
+    long double* norm = n_vector_generator(n);
+
     long double two_point_distance = 0.0l;
-    unsigned int index_sum = 0;
 
     for (unsigned int i_first_point = 0; i_first_point < npix; ++i_first_point) {
         for (unsigned int j_first_point = 1; j_first_point < npix / 2; ++j_first_point) {
-            for (int i_second_point = i_first_point; i_second_point < npix; ++i_second_point) {
+            for (unsigned int i_second_point = i_first_point; i_second_point < npix; ++i_second_point) {
                 for (unsigned int j_second_point = j_first_point; j_second_point < npix / 2; ++j_second_point) {
 
                     two_point_distance = s2(map_parameter * i_first_point, map_parameter * i_second_point,
                                             map_parameter * j_first_point, map_parameter * j_second_point);
 
-                    function[static_cast<unsigned int>(floorl(two_point_distance / PI * n))] +=
+                    function[static_cast<unsigned int>((two_point_distance / PI * n))] +=
                             map[i_first_point][j_first_point] * map[i_second_point][j_second_point];
 
-                    index_sum += 1;
-
-
+                    norm[static_cast<unsigned int>((two_point_distance / PI * n))] += 1;
                 }
             }
         }
     }
 
+    for (unsigned int i = 0; i < n; ++i) {
+        function[i] /= norm[i];
+    }
 
+    n_vector_destroyer(norm);
 
 }
