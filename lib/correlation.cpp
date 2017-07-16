30532e83b9cec3f963fb6fc5164790bc6245b44f
Ivan Edigarev <heyfaraday@gmail.com>
2017-07-17 00:49:21 +0300

    Correlation function with mask added

--- lib/correlation.cpp
+++ lib/correlation.cpp
@@ -42,3 +42,42 @@ void correlation_function(long double** map, unsigned int n, long double* functi
     n_vector_destroyer(norm);
 
 }
+
+
+void correlation_function_with_mask(long double** map, int** mask, unsigned int n, long double* function) {
+
+    long double* norm = n_vector_generator(n);
+
+    long double two_point_distance = 0.0l;
+
+    for (unsigned int i_first_point = 0; i_first_point < npix; ++i_first_point) {
+        for (unsigned int j_first_point = 1; j_first_point < npix / 2; ++j_first_point) {
+
+            if (mask[i_first_point][j_first_point] != 0) {
+
+                for (unsigned int i_second_point = i_first_point; i_second_point < npix; ++i_second_point) {
+                    for (unsigned int j_second_point = j_first_point; j_second_point < npix / 2; ++j_second_point) {
+
+                        if (mask[i_second_point][j_second_point] != 0) {
+
+                            two_point_distance = s2(map_parameter * i_first_point, map_parameter * i_second_point,
+                                                    map_parameter * j_first_point, map_parameter * j_second_point);
+
+                            function[static_cast<unsigned int>((two_point_distance / PI * n))] +=
+                                    map[i_first_point][j_first_point] * map[i_second_point][j_second_point];
+
+                            norm[static_cast<unsigned int>((two_point_distance / PI * n))] += 1;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    for (unsigned int i = 0; i < n; ++i) {
+        function[i] /= norm[i];
+    }
+
+    n_vector_destroyer(norm);
+
+}
--- lib/correlation.hpp
+++ lib/correlation.hpp
@@ -1,3 +1,5 @@
 #pragma once
 
 void correlation_function(long double** map, unsigned int n, long double* function);
+
+void correlation_function_with_mask(long double** map, int** mask, unsigned int n, long double* function);
